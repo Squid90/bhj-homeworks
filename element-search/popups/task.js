b221be0bca4bc43f9048842939fa612d69d167c4
Ivan Romaniuk <squid90@rambler.ru>
2023-01-15 19:44:42 +0300

    part done

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,14 +1,17 @@
 const modal = document.getElementById('modal_main')
 modal.className = modal.className + ' modal_active';
 
-const modalClose = document.querySelector('.modal__close');
-const parent = modalClose.closest('.modal_active');
+const modalClose = [...document.querySelectorAll('.modal__close')];
 const modalSucces = document.getElementById('modal_success');
 const showSucces = document.querySelector('.show-success');
 
-modalClose.onclick = function() {
-    parent.className = 'modal';
- };
+modalClose.forEach((element) => {
+    const parent = element.closest('.modal_active');
+        element.onclick = function() {
+            parent.className = 'modal';
+        };
+    }
+)
 
  showSucces.onclick = function() {
     modalSucces.className = modalSucces.className + ' modal_active';
