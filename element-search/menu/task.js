b5cbaa7656f77577b6e58984e67bde7ab34a76c2
Ivan Romaniuk <squid90@rambler.ru>
2023-01-17 23:58:30 +0300

    Menu done

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,14 +1,16 @@
-const menuLink = [...document.querySelectorAll('.menu__item')];
-
-
+const menuLink = [...document.querySelectorAll('.menu__link')];
 
 menuLink.forEach((element) => {
-   element.oncklick = function() {
-      
-      // const menuLinkSecond = [...document.querySelectorAll('.menu_sub')];
-      // menuLinkSecond.forEach((item) => {
-      //    item.classList.toggle('menu_active');
-      // });
-      return false;
+   element.onclick = function() {
+      const checkActive = document.querySelector('.menu_active');
+      const parents = element.closest('.menu__item');
+      const openMenu = parents.querySelector('.menu_sub');
+      if (checkActive !== null) {
+         checkActive.classList.toggle('menu_active');
+         openMenu.classList.toggle('menu_active');
+      } else {
+         openMenu.classList.toggle('menu_active');
+      }
+      return false     
    };
 });
