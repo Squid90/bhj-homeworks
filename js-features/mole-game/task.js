40ffc0f537c29b249d780fb3e1d54abc296152ea
Ivan Romaniuk <squid90@rambler.ru>
2023-01-19 22:37:48 +0300

    Mole-game Done

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -4,15 +4,8 @@ let lost = parseInt(document.getElementById('lost').textContent, 10);
 function getHole(index) {
     return document.getElementById(`hole${index}`)
 };
-function youWin(){
-    alert("You win")
-    dead = 0;
-    lost = 0;
-    document.getElementById('dead').textContent = dead;
-    document.getElementById('lost').textContent = lost;
-};
-function youLose(){
-    alert("You lose")
+function endGame(message){
+    alert(message);
     dead = 0;
     lost = 0;
     document.getElementById('dead').textContent = dead;
@@ -26,13 +19,13 @@ for (let index = 1; index < 10; index += 1) {
             dead = dead + 1;
             document.getElementById('dead').textContent = dead;
             if (dead === 10) {
-                youWin();
+                endGame("You win");
             };
         } else {
             lost = lost + 1;
             document.getElementById('lost').textContent = lost;
             if (lost === 5) {
-                youLose();
+                endGame("You lose");
             };
         };
     };
