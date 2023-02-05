3762de2ac3383ec25ad2f036bdaf91aa8bd9385d
Ivan Romaniuk <squid90@rambler.ru>
2023-02-05 20:34:36 +0300

    done

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -3,12 +3,13 @@ const tooltip = [...document.querySelectorAll('.has-tooltip')];
 tooltip.forEach((element) => {
     element.insertAdjacentHTML("afterEnd",
             '<div class="tooltip" >Текст подсказки</div>'
-    );//style="left: 0; top: 0"
+    );
     const nextTooltip = element.nextSibling;
+    const { top, left } = element.getBoundingClientRect();
 
     element.addEventListener('click', function() {
         const activeTag = document.querySelector('.tooltip_active');
-        
+
         if (activeTag !== null) {
             if (nextTooltip.classList.contains('tooltip_active')) {
                 nextTooltip.classList.remove('tooltip_active');
@@ -16,12 +17,12 @@ tooltip.forEach((element) => {
                 activeTag.classList.remove('tooltip_active');
                 nextTooltip.classList.add('tooltip_active');
                 nextTooltip.innerText = element.getAttribute('title');
-                nextTooltip.setAttribute('style', `left: ${0}; top: ${0}`);
+                nextTooltip.setAttribute('style', `left: ${10/*left*/}; top: ${10/*top*/}`);
             }
         } else {
             nextTooltip.classList.add('tooltip_active');
             nextTooltip.innerText = element.getAttribute('title');
-            nextTooltip.setAttribute('style', `left: ${0}; top: ${0}`);
+            nextTooltip.setAttribute('style', `left: ${10/*left*/}; top: ${10/*top*/}`);
         }
         
         
